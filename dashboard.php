47bc9def0a8f72685710fdea4d9a442b5270cb8d
cPoledoUP <capoledo@up.edu.ph>
2022-06-01 19:17:38 +0800

    Improved dashboard page

--- dashboard.php
+++ dashboard.php
@@ -1,148 +1,191 @@
-<?php
-
-?>
-
 <?php include('templates/head.php') ?>
-    <title>Staff Dashboard</title>
+    <title>Dashboard</title>
     <link rel="stylesheet" href="css/dashboard.css">
 <?php include('templates/nav.php') ?>
+    <div class="main-content">
+        <!-- Main page content -->
 
-    <div class="container">
-        <div class="c1">
-            <p1>Library Database Management</p1>
-            <div class="containerData">
-                <div class="searchRow">
-                    <div class="search select">
-                        <input type="text" placeholder="Search..." style="width:150%"> 
-                        <button class="blue"><i class="fa-solid fa-magnifying-glass"></i></button>
-                </div>
-                </div>
-                <div class="resultRow">
-                    <div class="bookResult">
-                        <p>Results for: Loren ipsum</p>
-                        <div class="trial">
-                        <div class="bookSec">
-                            <div class="pic">
-                                <img src="img/icons/cover-page.png" alt="book" >
-                                <div class="desc">
-                                    <p>Book A</p>
-                                    <p>Author: Author guy</p>
-                                    <p>Genre: A, b, c</p>
-                                </div>
-                            </div>
-                            <div class="pic">
-                                <img src="img/icons/cover-page.png" alt="book" >
-                                <div class="desc">
-                                    <p>Book A</p>
-                                    <p>Author: Author guy</p>
-                                    <p>Genre: A, b, c</p>
-                                </div>
-                            </div>
-                            <div class="pic">
-                                <img src="img/icons/cover-page.png" alt="book" >
-                                <div class="desc">
-                                    <p>Book A</p>
-                                    <p>Author: Author guy</p>
-                                    <p>Genre: A, b, c</p>
-                                </div>
-                            </div>   
-                            <div class="pic">
-                                <img src="img/icons/cover-page.png" alt="book" >
-                                <div class="desc">
-                                    <p>Book A</p>
-                                    <p>Author: Author guy</p>
-                                    <p>Genre: A, b, c</p>
-                                </div>
-                            </div>    
-                            <div class="pic">
-                                <img src="img/icons/cover-page.png" alt="book" >
-                                <div class="desc">
-                                    <p>Book A</p>
-                                    <p>Author: Author guy</p>
-                                    <p>Genre: A, b, c</p>
-                                </div>
-                            </div>  
-                            <div class="pic">
-                                <img src="img/icons/cover-page.png" alt="book" >
-                                <div class="desc">
-                                    <p>Book A</p>
-                                    <p>Author: Author guy</p>
-                                    <p>Genre: A, b, c</p>
-                                </div>
-                            </div>                   
-                        </div> 
-                        <div class="buttom">
-                            <button class="btn blue clickable">Add New Book</button>
-                        </div>
-                    </div> 
-                    </div>                   
-                    <div class="formSection">
-                        <div class="form1">
-                            <form>
-                                <div class="alignF"><input type="text" placeholder="Book Name" ></div>
-                                <div class="alignF"><input type="text" placeholder="Author"></div>
-                                <div class="alignF"><input type="text" placeholder="Genre"></div>
-                                <div class="alignF"><input type="text" placeholder="Publish Date"></div>
-                                <div class="alignF"><input type="text" placeholder="Publisher"></div>
-                                <div class="alignF"><input type="text" placeholder="Language"></div>
-                                <div class="alignF"><input type="text" placeholder="Pages"></div>
-                                <div class="alignF"><input type="text" placeholder="ISBN"></div>
-                                <div class="alignF"><input type="text" placeholder="Overview:"></div>
-    
-                            </form>
-                        </div>
-                        <div class="arrange">
-                            <button class="btn red">Remove</button>
-                            <button class="btn blue clickable">Save changes</button>
-                        </div>
-                        
-                    </div>
-                </div>
-            </div>
-            </div>
-        <div class="c2">
-            <p1>User Records Management</p1>
-            <div class="newUser"> 
-                <span class="details">For New Users </span>
-                    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-                        <div class="alignG"><input type="text" placeholder="Last Name" required></div>
-                        <div class="alignG"><input type="text" placeholder="First Name"required></div>
-                        <div class="alignG"><input type="text" placeholder="M I" required></div>
-                        <input type="text" placeholder="Address" style="width:100%" required>
-                       <div class="try"><input type="submit" name="register-user"></div>
-                    </form>
-            </div>
-            <div class="existingUser">
-                <span class="details">For Existing Users </span>
-                <div class="search1">
-                    <input class="librarySearch" type="text" placeholder="Library ID" style="width:92%"> 
+        <div class="lib-management card">
+            <h1>Library Database Management</h1>
+            <div class="inner-card">
+                
+                <div class="search">
+                    <input type="text" placeholder="Search Database"> 
                     <button class="blue"><i class="fa-solid fa-magnifying-glass"></i></button>
-                </div>
-                <span class="details"> User: 000000000001 - Juan Dela Cruz</span>
-                <div class="bookContainer"> 
-                    <div class="leftTitle">
-                        <img src="img/icons/cover-page.png" alt="Sample volume 3">
-                        <div class="borrowDetails">
-                           <p6>Book title</p6>
-                           <p6>Borrow: </p6>
-                           <p6>03/20/2021</p6>
-                           <p6>Returned:</p6>
-                           <p6>03/20/2021</p6>
-                        </div>
-                    </div>
-                    <div class="rightTitle">
-                        <img src="img/icons/cover-page.png" alt="Sample volume 3">
-                        <div class="borrowDetails">
-                        <p6>Book title</p6>
-                           <p6>Borrow: </p6>
-                           <p6>03/20/2021</p6>
-                            <button class="btn red">Return</button>
-                         </div>
-                    </div>
-                </div>
-                <span class="newBorrow"><button class="btn red">New Borrow</button></span>
+                    <p>Results for: lorem ipsum</p>
                 </div>
                 
+                <div class="div1">
+                    <div class="db list">
+                        <div class="book">
+                            <img src="img/icons/cover-page.png" alt="cover page" width="75px" height="100px">
+                            <p>
+                                Book ABook ABook ABook ABook ABook ABook ABook A<br>
+                                Author: Author<br>
+                                Genre: A, B<br>
+                            </p>
+                        </div>
+                        <div class="book">
+                            <img src="img/icons/cover-page.png" alt="cover page" width="75px" height="100px">
+                            <p>
+                                Book A<br>
+                                Author: Author<br>
+                                Genre: A, B<br>
+                            </p>
+                        </div>
+                        <div class="book">
+                            <img src="img/icons/cover-page.png" alt="cover page" width="75px" height="100px">
+                            <p>
+                                Book A<br>
+                                Author: Author<br>
+                                Genre: A, B<br>
+                            </p>
+                        </div>
+                        <div class="book">
+                            <img src="img/icons/cover-page.png" alt="cover page" width="75px" height="100px">
+                            <p>
+                                Book A<br>
+                                Author: Author<br>
+                                Genre: A, B<br>
+                            </p>
+                        </div>
+                        <div class="book">
+                            <img src="img/icons/cover-page.png" alt="cover page" width="75px" height="100px">
+                            <p>
+                                Book A<br>
+                                Author: Author<br>
+                                Genre: A, B<br>
+                            </p>
+                        </div>
+                        <div class="book">
+                            <img src="img/icons/cover-page.png" alt="cover page" width="75px" height="100px">
+                            <p>
+                                Book A<br>
+                                Author: Author<br>
+                                Genre: A, B<br>
+                            </p>
+                        </div>
+                        <div class="book">
+                            <img src="img/icons/cover-page.png" alt="cover page" width="75px" height="100px">
+                            <p>
+                                Book A<br>
+                                Author: Author<br>
+                                Genre: A, B<br>
+                            </p>
+                        </div>
+                        <div class="book">
+                            <img src="img/icons/cover-page.png" alt="cover page" width="75px" height="100px">
+                            <p>
+                                Book A<br>
+                                Author: Author<br>
+                                Genre: A, B<br>
+                            </p>
+                        </div>
+                    </div>
+
+                    <form class="center"><button class="btn blue clickable">Add New Book</button></form>
+                </div>
+
+                <form action="" class="div1">
+                    <input type="text" placeholder="Book Name" >
+                    <input type="text" placeholder="Author">
+                    <input type="text" placeholder="Genre">
+                    <input type="text" placeholder="Publish Date">
+                    <input type="text" placeholder="Publisher">
+                    <input type="text" placeholder="Language">
+                    <input type="text" placeholder="Pages">
+                    <input type="text" placeholder="ISBN">
+                    <textarea placeholder="Overview:" rows="5"></textarea>
+                    <div class="center">
+                        <button class="btn red clickable">Remove</button>
+                        <button class="btn blue clickable">Save changes</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="user-management card">
+            <h1>User Records Management</h1>
+
+            <div class="inner-card user-reg">
+                <h3>For New Users</h3>
+                <form action="">
+                    <input type="text" placeholder="Last Name">
+                    <input type="text" placeholder="First Name">
+                    <input type="text" placeholder="M.I.">
+                    <input type="text" placeholder="Address">
+                    <div class="right"><button class="btn blue clickable">Register User</button></div>
+                </form>
+            </div>
+
+            <div class="inner-card">
+                <h3>For Existing Users</h3>
+                <div class="search">
+                    <input type="text" placeholder="Search Database"> 
+                    <button class="blue"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+
+                <h4>User: 1 - Juan Dela Cruz</h4>
+
+                <div class="borrow list">
+                    <div class="book">
+                        <img src="img/icons/cover-page.png" alt="cover page" width="75px" height="100px">
+                        <p>
+                            Book ABook ABook ABook ABook ABook ABook ABook A<br>
+                            Borrowed:<br>
+                            03/20/2022<br>
+                            Returned:<br>
+                            03/27/2022<br>
+                        </p>
+                    </div>
+                    <div class="book">
+                        <img src="img/icons/cover-page.png" alt="cover page" width="75px" height="100px">
+                        <p>
+                            Book ABook ABook ABook ABook ABook ABook ABook A<br>
+                            Borrowed:<br>
+                            03/20/2022<br>
+                            <button class="btn red clickable">Return</button>
+                        </p>
+                    </div>
+                    <div class="book">
+                        <img src="img/icons/cover-page.png" alt="cover page" width="75px" height="100px">
+                        <p>
+                            Book ABook ABook ABook ABook ABook ABook ABook A<br>
+                            Borrowed:<br>
+                            03/20/2022<br>
+                            Returned:<br>
+                            03/27/2022<br>
+                        </p>
+                    </div>
+                    <div class="book">
+                        <img src="img/icons/cover-page.png" alt="cover page" width="75px" height="100px">
+                        <p>
+                            Book ABook ABook ABook ABook ABook ABook ABook A<br>
+                            Borrowed:<br>
+                            03/20/2022<br>
+                            <button class="btn red clickable">Return</button>
+                        </p>
+                    </div>
+                    <div class="book">
+                        <img src="img/icons/cover-page.png" alt="cover page" width="75px" height="100px">
+                        <p>
+                            Book ABook ABook ABook ABook ABook ABook ABook A<br>
+                            Borrowed:<br>
+                            03/20/2022<br>
+                            Returned:<br>
+                            03/27/2022<br>
+                        </p>
+                    </div>
+                    <div class="book">
+                        <img src="img/icons/cover-page.png" alt="cover page" width="75px" height="100px">
+                        <p>
+                            Book ABook ABook ABook ABook ABook ABook ABook A<br>
+                            Borrowed:<br>
+                            03/20/2022<br>
+                            <button class="btn red clickable">Return</button>
+                        </p>
+                    </div>
+                </div>
+                <form class="right"><button class="btn blue clickable">New Borrow</button></form>
             </div>
         </div>
     </div>
