d4ea90cd7919eb1745d1e101d5a1ac96225c4e27
cPoledoUP <capoledo@up.edu.ph>
2022-06-05 13:13:33 +0800

    revamped login card when staff is logged in

--- utils/login.php
+++ utils/login.php
@@ -19,5 +19,21 @@ if (isset($_POST['login'])) {
     }
 }
 
+// behavior if logged in
+elseif (isset($_SESSION['staffID'])) {
+
+    if(isset($_POST['dashboard'])) {
+        header('Location: dashboard.php');
+    } elseif(isset($_POST['logout'])) {
+        header('Location: utils/logout.php');
+    }
+    
+    $staff_id = $_SESSION['staffID'];
+    $query = 'SELECT FirstName, LastName FROM staff WHERE StaffID='.mysqli_real_escape_string($conn, $staff_id);
+    $result = mysqli_query($conn, $query);
+    $staff_name = mysqli_fetch_assoc($result);
+    mysqli_free_result($result);
+}
+
 mysqli_close($conn);
 ?>
