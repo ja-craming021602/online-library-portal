3602bbd4134c39d31d26bf6e5ba0e2b21c848d85
cPoledoUP <capoledo@up.edu.ph>
2022-06-05 13:58:31 +0800

    Made login input fields required

--- templates/logged-out.php
+++ templates/logged-out.php
@@ -2,8 +2,8 @@
                     <p1>Staff Login</p1>
                     <form name="login" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
                         <div class="input">
-                            <input type="text" name="staffID" placeholder="Staff ID"><br><br>
-                            <input type="password" name="password" placeholder="Password"><br><br>
+                            <input type="text" name="staffID" placeholder="Staff ID" required><br><br>
+                            <input type="password" name="password" placeholder="Password" required><br><br>
                             <input type="submit" name="login" class="btn blue clickable" value="Login">
                         </div>
                     </form>
