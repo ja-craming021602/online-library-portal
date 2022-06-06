e9cb9bb2885f79afe97ce504a7093b4d043de50c
cPoledoUP <capoledo@up.edu.ph>
2022-06-06 16:27:31 +0800

    Prettified the codes

--- bookpage.php
+++ bookpage.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 // establish connection
 include('utils/connection.php');
 
-if(isset($_GET['ID'])){
+if (isset($_GET['ID'])) {
     $ID = mysqli_real_escape_string($conn, $_GET['ID']);
     $query_book = "SELECT * FROM book WHERE BookID = '$ID'";
     $query_genre = "SELECT Genre FROM rl_book_genre WHERE BookID = '$ID'";
     $query_author = "SELECT Author FROM rl_book_author WHERE BookID = '$ID'";
     $query_borrow = "SELECT DateReturned FROM borrow WHERE BookID = '$ID'";
-    
+
     $book_result = mysqli_query($conn, $query_book);
     $genre_result = mysqli_query($conn, $query_genre);
     $author_result = mysqli_query($conn, $query_author);
@@ -18,7 +18,7 @@ if(isset($_GET['ID'])){
     $genres = mysqli_fetch_all($genre_result, MYSQLI_ASSOC);
     $authors = mysqli_fetch_all($author_result, MYSQLI_ASSOC);
     $borrow = mysqli_fetch_all($borrow_result, MYSQLI_ASSOC);
-    
+
     foreach ($authors as $author) {
         $book_details['Authors'][] = $author['Author'];
     }
@@ -40,31 +40,29 @@ if(isset($_GET['ID'])){
     mysqli_free_result($genre_result);
     mysqli_free_result($author_result);
     mysqli_free_result($borrow_result);
-
 }
 
 
 ?>
 <?php include('templates/head.php') ?>
-    <title><?php echo htmlspecialchars($book_details['Title']); ?></title>
-    <link rel="stylesheet" href="css/bookpage.css">
-<?php include('templates/nav.php') ?>     
+<title><?php echo htmlspecialchars($book_details['Title']); ?></title>
+<link rel="stylesheet" href="css/bookpage.css">
+<?php include('templates/nav.php') ?>
 
-    <?php if(isset($_GET['ID'])): ?>
-    <div class="container">     
-        <div class="sidebarL"> 
+<?php if (isset($_GET['ID'])) : ?>
+    <div class="container">
+        <div class="sidebarL">
             <div class="pictureBar">
                 <img src="img/icons/cover-page.png" alt="Sample volume 3">
-                <button class="btn blue">
-                    <?php 
-                    if(!$available){
-                        echo "Unavailable"; 
-                    }else{
-                        echo "Available in Library";
-                    }
-                ?>
-                </button>
-                </div>
+                <?php if ($available) : ?>
+                    <button class="btn blue">
+                        Available in Library
+                <?php else : ?>
+                    <button class="btn red">
+                        Not Available in Library
+                <?php endif; ?>
+                    </button>
+            </div>
             <div class="shareBar">
                 <p3>Share this Book:</p3><br>
                 <button class="btn blue clickable circle fb"><i class="fa-brands fa-facebook"></i> </button>
@@ -73,19 +71,21 @@ if(isset($_GET['ID'])){
                 <button class="btn blue clickable circle reddit"><i class="fa brands fa-reddit-alien"></i></button>
             </div>
         </div>
-<!------------------------------------MTRIAL---------------------------------------------->
+        <!------------------------------------MTRIAL---------------------------------------------->
         <div class="mainBar">
             <div class="titleBar">
                 <div class="mainTitle">
-                    <p10><?php echo $book_details['Title'];?></p10>
+                    <p10><?php echo $book_details['Title']; ?></p10>
                     <p9>
-                       <p9>By</p9>
-                    <?php echo htmlspecialchars(implode(', ', $book_details['Authors'])); ?>
+                        <p9>By</p9>
+                        <?php echo htmlspecialchars(implode(', ', $book_details['Authors'])); ?>
                     </p9>
                 </div>
                 <div class="hyperlinks">
-                    <a class="one" href="https://i.redd.it/29pgg3ifyvp21.png">Borrow history</a><!---- link for USER HISTORY -->
-                    <a class="two" href="https://c.tenor.com/qQk6DhSV1qUAAAAd/anime-grand.gif">Edit</a><!---- link for DASHBOARD -->
+                    <a class="one" href="https://i.redd.it/29pgg3ifyvp21.png">Borrow history</a>
+                    <!---- link for USER HISTORY -->
+                    <a class="two" href="https://c.tenor.com/qQk6DhSV1qUAAAAd/anime-grand.gif">Edit</a>
+                    <!---- link for DASHBOARD -->
                 </div>
             </div>
             <div class="publishBar">
@@ -93,83 +93,80 @@ if(isset($_GET['ID'])){
                 <p3>Publisher</p3>
                 <p3>Language</p3>
                 <p3>Pages </p3>
-                <p2><?php echo $book_details['PubDate'];?> </p2>
-                <p2><?php echo $book_details['Publisher'];?> </p2>
-                <p2><?php echo $book_details['Language'];?> </p2>
-                <p2><?php echo $book_details['PageCount'];?> </p2>
+                <p2><?php echo $book_details['PubDate']; ?> </p2>
+                <p2><?php echo $book_details['Publisher']; ?> </p2>
+                <p2><?php echo $book_details['Language']; ?> </p2>
+                <p2><?php echo $book_details['PageCount']; ?> </p2>
             </div>
             <div class="overBar">
                 <p3>Overview</p3>
-                <p2><?php echo $book_details['Overview'];?></p2>
+                <p2><?php echo $book_details['Overview']; ?></p2>
             </div>
         </div>
-<!------------------------------------MTRIAL---------------------------------------------->
+        <!------------------------------------MTRIAL---------------------------------------------->
         <div class="sidebarR">
             <div class="detailBar">
                 <h2>Book Details</h2>
             </div>
             <div class="isbnBar">
                 <p3>ISBN</p3>
-                <p2><?php echo $book_details['ISBN'];?></p2>
+                <p2><?php echo $book_details['ISBN']; ?></p2>
             </div>
             <div class="genreBar">
                 <p3>Genre</p3>
-                <?php if($book_details['Genres']): ?>
-                <?php echo htmlspecialchars(implode(', ', $book_details['Genres'])) ?>
+                <?php if ($book_details['Genres']) : ?>
+                    <?php echo htmlspecialchars(implode(', ', $book_details['Genres'])) ?>
 
-                <?php else: ?>
+                <?php else : ?>
                     <p3> None</p3>
-                    <?php endif; ?>
-                
+                <?php endif; ?>
+
             </div>
             <div class="ratingBar">
                 <p3>User Ratings</p3>
                 <div class="row">
-                <p2><?php echo $book_details['Rating'];?></p2>
+                    <p2><?php echo $book_details['Rating']; ?></p2>
                 </div>
             </div>
         </div>
-        
-    </div>  
-    <?php else: ?> <!------ DISPALY ERROR PAGE if requested id not found-->
-            <p> Error </p>
-            <p> Requested page not found </p>
-        <?php endif; ?>  
 
-    
+    </div>
+<?php else : ?>
+    <!------ DISPALY ERROR PAGE if requested id not found-->
+    <p> Error </p>
+    <p> Requested page not found </p>
+<?php endif; ?>
 
-    <script>
-        const pageURL = location.href
-        const fb = document.querySelector('.fb')
-        const twitter =  document.querySelector('.twitter')
-        const telegram = document.querySelector('.telegram')
-        const reddit = document.querySelector('.reddit')
 
-        const twitterApi = `https://twitter.com/intent/tweet?text=${pageURL}`;
-        const fbApi = `https://www.facebook.com/sharer/sharer.php?u=${pageURL}`
-        console.log(pageURL)
-        const telegramApi = `https://t.me/share/url?url=${pageURL}`
-        const redditApi = `https://www.reddit.com/submit?url=${pageURL}`
 
-        fb.addEventListener('click', ()=>{
-            window.open(url = fbApi, target='blank')
-        })
+<script>
+    const pageURL = location.href
+    const fb = document.querySelector('.fb')
+    const twitter = document.querySelector('.twitter')
+    const telegram = document.querySelector('.telegram')
+    const reddit = document.querySelector('.reddit')
 
-        twitter.addEventListener('click', ()=>{
-            window.open(url = twitterApi, target='blank')
-        })
+    const twitterApi = `https://twitter.com/intent/tweet?text=${pageURL}`;
+    const fbApi = `https://www.facebook.com/sharer/sharer.php?u=${pageURL}`
+    console.log(pageURL)
+    const telegramApi = `https://t.me/share/url?url=${pageURL}`
+    const redditApi = `https://www.reddit.com/submit?url=${pageURL}`
 
-        telegram.addEventListener('click', ()=>{
-            window.open(url = telegramApi, target='blank')
-        })
+    fb.addEventListener('click', () => {
+        window.open(url = fbApi, target = 'blank')
+    })
 
-        reddit.addEventListener('click', ()=>{
-            window.open(url = redditApi, target='blank')
-        })
+    twitter.addEventListener('click', () => {
+        window.open(url = twitterApi, target = 'blank')
+    })
 
-        
+    telegram.addEventListener('click', () => {
+        window.open(url = telegramApi, target = 'blank')
+    })
 
-    
-        </script>
+    reddit.addEventListener('click', () => {
+        window.open(url = redditApi, target = 'blank')
+    })
+</script>
 
 <?php include('templates/footer.php') ?>
--- user-history.php
+++ user-history.php
@@ -1,57 +1,57 @@
 <?php
-    include('utils/connection.php');
+include('utils/connection.php');
 
-    if (isset($_GET['search-user'])) {
-        $user_id = mysqli_real_escape_string($conn, $_GET['user-id']);
+if (isset($_GET['search-user'])) {
+    $user_id = mysqli_real_escape_string($conn, $_GET['user-id']);
 
-        // validate
-        if (!ctype_digit($user_id) || (int)$user_id < 1) {
-            echo "<script>alert('Invalid user ID! Must be a positive integer.')</script>";
-            unset($_GET['search-user']);
-        } else {
-            $query = "SELECT BookID, DateOfBorrow, DateReturned, DueDate FROM borrow WHERE UserID=$user_id";
-            $result = mysqli_query($conn, $query);
-            $borrow_array = mysqli_fetch_all($result, MYSQLI_ASSOC);
-            mysqli_free_result($result);
-            
-            // retrieving user's name
-            $query = "SELECT FirstName, LastName FROM user WHERE UserID=$user_id";
-            $result = mysqli_query($conn, $query);
-            $user_name = mysqli_fetch_assoc($result);
-            mysqli_free_result($result);
+    // validate
+    if (!ctype_digit($user_id) || (int)$user_id < 1) {
+        echo "<script>alert('Invalid user ID! Must be a positive integer.')</script>";
+        unset($_GET['search-user']);
+    } else {
+        $query = "SELECT BookID, DateOfBorrow, DateReturned, DueDate FROM borrow WHERE UserID=$user_id";
+        $result = mysqli_query($conn, $query);
+        $borrow_array = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        mysqli_free_result($result);
 
-            if ($user_name != NULL) {
+        // retrieving user's name
+        $query = "SELECT FirstName, LastName FROM user WHERE UserID=$user_id";
+        $result = mysqli_query($conn, $query);
+        $user_name = mysqli_fetch_assoc($result);
+        mysqli_free_result($result);
 
-                // retrieving book titles
-                foreach ($borrow_array as &$borrow) {
-                    $query = "SELECT Title FROM book WHERE BookID=${borrow['BookID']}";
-                    $result = mysqli_query($conn, $query);
-                    $book_title = mysqli_fetch_assoc($result);
-                    mysqli_free_result($result);
-                    $borrow['Title'] = $book_title['Title'];
-                }
-                unset($borrow);
+        if ($user_name != NULL) {
 
-                // retrieving book authors
-                foreach ($borrow_array as &$borrow) {
-                    $query = "SELECT Author FROM rl_book_author WHERE BookID=${borrow['BookID']}";
-                    $result = mysqli_query($conn, $query);
-                    $authors = mysqli_fetch_all($result, MYSQLI_ASSOC);
-                    mysqli_free_result($result);
-
-                    foreach ($authors as $author) {
-                        $borrow['Authors'][] = $author['Author'];
-                    }
-                }
-                unset($borrow);
-            } else {
-                echo "<script>alert('User not found.')</script>";
-                unset($_GET['search-user']);
+            // retrieving book titles
+            foreach ($borrow_array as &$borrow) {
+                $query = "SELECT Title FROM book WHERE BookID=${borrow['BookID']}";
+                $result = mysqli_query($conn, $query);
+                $book_title = mysqli_fetch_assoc($result);
+                mysqli_free_result($result);
+                $borrow['Title'] = $book_title['Title'];
             }
+            unset($borrow);
+
+            // retrieving book authors
+            foreach ($borrow_array as &$borrow) {
+                $query = "SELECT Author FROM rl_book_author WHERE BookID=${borrow['BookID']}";
+                $result = mysqli_query($conn, $query);
+                $authors = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                mysqli_free_result($result);
+
+                foreach ($authors as $author) {
+                    $borrow['Authors'][] = $author['Author'];
+                }
+            }
+            unset($borrow);
+        } else {
+            echo "<script>alert('User not found.')</script>";
+            unset($_GET['search-user']);
         }
     }
+}
 
-    mysqli_close($conn);
+mysqli_close($conn);
 ?>
 
 <?php include('templates/head.php') ?>
@@ -70,28 +70,36 @@
             <div><button class="btn black clickable" name="search-user">Submit</i></button></div>
         </form>
 
-        <?php if (isset($_GET['search-user'])): ?>
+        <?php if (isset($_GET['search-user'])) : ?>
 
-        <div class="card">
-            <p class="inner-bubble center-text">
-                <i class="fa-solid fa-triangle-exclamation"></i>User Information Found!
-            </p>
-            <div class="user-info">
-                <b>Last Name </b> <p class="inner-bubble"><?php echo htmlspecialchars($user_name['LastName']); ?></p>
-                <b>First Name</b> <p class="inner-bubble"><?php echo htmlspecialchars($user_name['FirstName']); ?></p>
-                <b>User ID   </b> <p class="inner-bubble"><?php echo htmlspecialchars($user_id); ?></p>
+            <div class="card">
+                <p class="inner-bubble center-text">
+                    <i class="fa-solid fa-triangle-exclamation"></i>User Information Found!
+                </p>
+                <div class="user-info">
+                    <b>Last Name </b>
+                    <p class="inner-bubble"><?php echo htmlspecialchars($user_name['LastName']); ?></p>
+                    <b>First Name</b>
+                    <p class="inner-bubble"><?php echo htmlspecialchars($user_name['FirstName']); ?></p>
+                    <b>User ID </b>
+                    <p class="inner-bubble"><?php echo htmlspecialchars($user_id); ?></p>
+                </div>
             </div>
-        </div>
 
         <?php endif; ?>
 
     </div>
 
-    <?php if (isset($_GET['search-user'])): ?>
-    
+    <?php if (isset($_GET['search-user'])) : ?>
+
         <div class="borrow-area card">
-            <div class="center"><h2><i class="fa-solid fa-box-archive"></i> User Track Record</h2></div>
-            <div class="center"><h3><?php echo htmlspecialchars(count($borrow_array)).' Result'; if (count($borrow_array) > 1) echo 's'; ?></h3></div>
+            <div class="center">
+                <h2><i class="fa-solid fa-box-archive"></i> User Track Record</h2>
+            </div>
+            <div class="center">
+                <h3><?php echo htmlspecialchars(count($borrow_array)) . ' Result';
+                    if (count($borrow_array) > 1) echo 's'; ?></h3>
+            </div>
 
             <div class="inner-bubble hidden-column-label">
                 <div class="label">Title</div>
@@ -102,7 +110,7 @@
 
             <div class="borrow-list">
 
-                <?php foreach ($borrow_array as $borrow): ?>
+                <?php foreach ($borrow_array as $borrow) : ?>
 
                     <div class="inner-bubble">
                         <div class="hidden-label">Title: </div>
@@ -113,16 +121,16 @@
                         <div class="label"><?php echo htmlspecialchars($borrow['DateOfBorrow']); ?></div>
                         <div class="hidden-label">Returned: </div>
 
-                        <?php if (isset($borrow['DateReturned'])): ?>
+                        <?php if (isset($borrow['DateReturned'])) : ?>
 
                             <div class="label"><?php echo htmlspecialchars($borrow['DateReturned']); ?></div>
 
-                        <?php else: ?>
+                        <?php else : ?>
 
                             <div class="label"><button class="btn red">Due: <?php echo htmlspecialchars($borrow['DueDate']); ?></button></div>
 
                         <?php endif; ?>
-                    
+
                     </div>
 
                 <?php endforeach; ?>
