986d9267ecd491c3d51ccd0d0fed5cc471de4ea4
cPoledoUP <capoledo@up.edu.ph>
2022-06-06 16:34:43 +0800

    Added alert message for illegal entry

--- utils/session.php
+++ utils/session.php
@@ -1,5 +1,6 @@
 <?php
     if (!isset($_SESSION["staffID"])) {
+        $_SESSION['illegal-access'] = 'Restricted Access. If you are a staff, log-in first!';
         header("location: index.php");
     }
 ?>
